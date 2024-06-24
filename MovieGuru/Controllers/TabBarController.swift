1594d8efc13f39d95cc1dc15a301d97995d671d6
Denys Bondarenko <denibondarenko@outlook.com>
2024-06-24 14:13:27 +0300

    Initial Xcode Project Setup and Structure

---
+++ MovieGuru/Controllers/TabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  TabBarController.swift
+//  MovieGuru
+//
+//  Created by Denys Bondarenko on 24.06.2024.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupAppearance()
+        setupTabBar()
+    }
+    
+    private func setupAppearance() {
+           view.backgroundColor = .yellow
+       }
+    
+    private func setupTabBar() {
+            let watchlistVC = WatchlistViewController()
+            let chatVC = ChatAIViewController()
+            let profileVC = ProfileViewController()
+
+            watchlistVC.navigationItem.largeTitleDisplayMode = .automatic
+            chatVC.navigationItem.largeTitleDisplayMode = .automatic
+            profileVC.navigationItem.largeTitleDisplayMode = .automatic
+
+            let nav1 = UINavigationController(rootViewController: watchlistVC)
+            let nav2 = UINavigationController(rootViewController: chatVC)
+            let nav3 = UINavigationController(rootViewController: profileVC)
+
+            nav1.tabBarItem = UITabBarItem(title: "Watchlist", image: UIImage(systemName: "square.3.layers.3d.down.left"), tag: 0)
+            nav2.tabBarItem = UITabBarItem(title: "AIFinder", image: UIImage(systemName: "ellipsis.message"), tag: 1)
+            nav3.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 2)
+
+            [nav1, nav2, nav3].forEach { nav in
+                nav.navigationBar.prefersLargeTitles = true
+            }
+
+            setViewControllers([nav1, nav2, nav3], animated: true)
+        }
+    
+
+}
---
+++ MovieGuru/Controllers/WatchlistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WatchlistViewController.swift
+//  MovieGuru
+//
+//  Created by Denys Bondarenko on 24.06.2024.
+//
+
+import UIKit
+
+class WatchlistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        title = "Watchlist"
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
